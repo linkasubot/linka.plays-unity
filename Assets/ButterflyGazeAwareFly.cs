3064bcbd15d14b481c4ad85b0af893d3e2964c18
Ivan Bakaidov <ivan@aacidov.ru>
2022-01-20 15:41:27 +0300

    add basic butterfly

---
+++ Assets/ButterflyGazeAwareFly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Tobii.Gaming;
+using UnityEngine;
+
+public class ButterflyGazeAwareFly : MonoBehaviour
+{
+    GazeAware gaze;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        gaze = GetComponent<GazeAware>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("fly", gaze.HasGazeFocus);
+    }
+
+}
